1f49bcaecaceb549ce8c36c898e6a1fdf95d53c1
ayoub129 <ayoub.berouijel10@gmail.com>
2022-08-31 18:06:29 +0200

    user backends

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require("./config/db")
 const PORT = process.env.PORT || 5000
 
+// connect to db
+connectDB()
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/' , (req , res) => {
     res.status(200).json({message: 'Welcome to the gulag'})
@@ -11,5 +20,7 @@ app.get('/' , (req , res) => {
 
 // Routes
 app.use('/api/users' , require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT , () => console.log(`server started on port ${PORT}`))
